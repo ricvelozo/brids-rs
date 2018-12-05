bd3432d6219a32538f624e92e9aa530eac895fac
Ricardo Silva Veloso <ricvelozo@gmail.com>
2018-12-05 06:03:56 -0200

    Update license

--- examples/random.rs
+++ examples/random.rs
@@ -1,10 +1,14 @@
-// Copyright 2018 Ricardo Silva Veloso
+// random.rs
+//
+// Copyright 2018 Ricardo Silva Veloso <ricvelozo@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT License
 // <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
+//
+// SPDX-License-Identifier: (MIT OR Apache-2.0)
 
 extern crate brids;
 
