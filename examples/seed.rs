2c612f738a4efc716bebc9d4a457e18011bf048d
Ricardo Silva Veloso <ricvelozo@gmail.com>
2019-10-25 17:47:52 -0300

    Update dependencies

--- examples/seed.rs
+++ examples/seed.rs
@@ -11,10 +11,10 @@
 // SPDX-License-Identifier: (MIT OR Apache-2.0)
 
 use brids::{Cnpj, Cpf};
-use rand::prelude::*;
+use rand::{SeedableRng, Rng, rngs::StdRng};
 
 fn main() {
-    let mut rng = SmallRng::seed_from_u64(123);
+    let mut rng = StdRng::seed_from_u64(123);
     println!("Random CNPJ number: {}", rng.gen::<Cnpj>());
     println!("Random CPF number: {}", rng.gen::<Cpf>());
 }
